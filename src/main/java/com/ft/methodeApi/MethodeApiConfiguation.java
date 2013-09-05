e5e1cb42b0d3f6b2c6548e2bff7fde18e3ae1ec6
Neil Green <neil@melessoftware.com>
2013-09-05 13:42:02 +0100

    runnable dropwizard instance

---
+++ src/main/java/com/ft/methodeApi/MethodeApiConfiguation.java
@@ -0,0 +1,6 @@
+package com.ft.methodeApi;
+
+import com.yammer.dropwizard.config.Configuration;
+
+public class MethodeApiConfiguation extends Configuration {
+}
---
+++ src/main/java/com/ft/methodeApi/MethodeApiService.java
@@ -0,0 +1,20 @@
+package com.ft.methodeApi;
+
+import com.yammer.dropwizard.Service;
+import com.yammer.dropwizard.config.Bootstrap;
+import com.yammer.dropwizard.config.Environment;
+
+public class MethodeApiService extends Service<MethodeApiConfiguation> {
+
+    public static void main(String[] args) throws Exception {
+        new MethodeApiService().run(args);
+    }
+
+    @Override
+    public void initialize(Bootstrap<MethodeApiConfiguation> bootstrap) {
+    }
+
+    @Override
+    public void run(MethodeApiConfiguation configuration, Environment environment) throws Exception {
+    }
+}
