84d64765daa3f79b159db898467d5b8f436a4c15
Neil Green <neil@melessoftware.com>
2013-09-05 17:56:05 +0100

    return optional from content resource

--- src/main/java/com/ft/methodeapi/service/ContentResource.java
+++ src/main/java/com/ft/methodeapi/service/ContentResource.java
@@ -12,48 +12,49 @@ import EOM.Session;
 import com.ft.methodeapi.connectivity.EomRepositoryFactory;
 import com.ft.methodeapi.connectivity.EomSessionFactory;
 import com.ft.methodeapi.connectivity.EomSessionWrapper;
+import com.google.common.base.Optional;
 import com.yammer.metrics.annotation.Timed;
 
 @Path("content")
 @Produces(MediaType.APPLICATION_JSON)
 public class ContentResource {
 
-	private final String methodeHostName;
-	private final int methodePort;
-	private final String methodeUserName;
-	private final String methodePassword;
+    private final String methodeHostName;
+    private final int methodePort;
+    private final String methodeUserName;
+    private final String methodePassword;
 
-	public ContentResource(String methodeHostName, int methodePort, String methodeUserName, String methodePassword) {
-		this.methodeHostName = methodeHostName;
-		this.methodePort = methodePort;
-		this.methodeUserName = methodeUserName;
-		this.methodePassword = methodePassword;
-	}
+    public ContentResource(String methodeHostName, int methodePort, String methodeUserName, String methodePassword) {
+        this.methodeHostName = methodeHostName;
+        this.methodePort = methodePort;
+        this.methodeUserName = methodeUserName;
+        this.methodePassword = methodePassword;
+    }
 
-	@GET
+    @GET
     @Timed
     @Path("{uuid}")
-    public Content getContent(@PathParam("uuid") String uuid) throws Exception {
-		try (EomRepositoryFactory repositoryFactory = new EomRepositoryFactory(methodeHostName, methodePort);
-			 EomSessionWrapper sessionWrapper = new EomSessionFactory(methodeUserName, methodePassword, repositoryFactory.createRepository()).createSession()) {
-			Session eomSession = sessionWrapper.getSession();
+    public Optional<Content> getContent(@PathParam("uuid") String uuid) throws Exception {
+        try (EomRepositoryFactory repositoryFactory = new EomRepositoryFactory(methodeHostName, methodePort);
+             EomSessionWrapper sessionWrapper = new EomSessionFactory(methodeUserName, methodePassword, repositoryFactory.createRepository()).createSession()) {
+            Session eomSession = sessionWrapper.getSession();
 
-			FileSystemAdmin fileSystemAdmin = EOM.FileSystemAdminHelper
-					.narrow(eomSession
+            FileSystemAdmin fileSystemAdmin = EOM.FileSystemAdminHelper
+                    .narrow(eomSession
                             .resolve_initial_references("FileSystemAdmin"));
 
-			String uri = "eom:/uuids/" + uuid;
+            String uri = "eom:/uuids/" + uuid;
 
-			FileSystemObject fso = fileSystemAdmin.get_object_with_uri(uri);
-			String type = fso.get_type_name();
+            FileSystemObject fso = fileSystemAdmin.get_object_with_uri(uri);
+            String type = fso.get_type_name();
 
-			if ("EOM::Story".equals(type) || "EOM::CompoundStory".equals(type)) {
-				EOM.File eomFile = EOM.FileHelper.narrow(fso);
-				Content content = new Content(eomFile.read_all());
-				return content;
-			} else {
-				return null;
-			}
-		}
-	}
+            if ("EOM::Story".equals(type) || "EOM::CompoundStory".equals(type)) {
+                EOM.File eomFile = EOM.FileHelper.narrow(fso);
+                Content content = new Content(eomFile.read_all());
+                return Optional.of(content);
+            } else {
+                return Optional.absent();
+            }
+        }
+    }
 }
