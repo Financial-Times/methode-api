7e9f40ab845b8ce0b525ab9655eb9b3bb20081c5
Neil Green <neil@melessoftware.com>
2013-09-09 12:21:32 +0100

    improve client config

--- methode-api-client/src/main/java/com/ft/methodeapi/client/MethodeApiClient.java
+++ methode-api-client/src/main/java/com/ft/methodeapi/client/MethodeApiClient.java
@@ -6,6 +6,8 @@ import javax.ws.rs.core.UriBuilder;
 
 import com.ft.methodeapi.model.EomFile;
 import com.sun.jersey.api.client.Client;
+import com.yammer.dropwizard.client.JerseyClientBuilder;
+import com.yammer.dropwizard.config.Environment;
 
 public class MethodeApiClient {
 
@@ -28,15 +30,38 @@ public class MethodeApiClient {
     }
 
     public EomFile findFileByUuid(String uuid) {
-        URI findFileByUuidUri = findFileByUuidUri(uuid);
         return jerseyClient
-                .resource(findFileByUuidUri)
+                .resource(findFileByUuidUri(uuid))
                 .accept(MediaType.APPLICATION_JSON_TYPE)
                 .get(EomFile.class);
     }
 
     private URI findFileByUuidUri(String uuid) {
-        return findFileByUuidUriBuilder.clone().build(uuid);
+        return findFileByUuidUriBuilder.clone().build(uuid); // It looks like build(...) isn't safe for concurrent use, but clone() is
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private MethodeApiClientConfiguration configuration;
+        private JerseyClientBuilder jerseyClientBuilder = new JerseyClientBuilder();
+
+        public Builder using(MethodeApiClientConfiguration configuration) {
+            this.configuration = configuration;
+            jerseyClientBuilder.using(configuration.getJerseyClientConfiguration());
+            return this;
+        }
+
+        public Builder using(Environment environment) {
+            jerseyClientBuilder.using(environment);
+            return this;
+        }
+
+        public MethodeApiClient build() {
+            return new MethodeApiClient(jerseyClientBuilder.build(), configuration.getMethodeApiHost(), configuration.getMethodeApiPort());
+        }
     }
 
 }
--- methode-api-client/src/main/java/com/ft/methodeapi/client/MethodeApiClientConfiguration.java
+++ methode-api-client/src/main/java/com/ft/methodeapi/client/MethodeApiClientConfiguration.java
@@ -1,16 +1,23 @@
 package com.ft.methodeapi.client;
 
+import javax.validation.Valid;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.base.Optional;
+import com.yammer.dropwizard.client.JerseyClientConfiguration;
 
 public class MethodeApiClientConfiguration {
 
     private final String methodeApiHost;
     private final int methodeApiPort;
+    private final JerseyClientConfiguration jerseyClientConfiguration;
 
     public MethodeApiClientConfiguration(@JsonProperty("apiHost") String methodeApiHost,
-                                         @JsonProperty("apiPort") int methodeApiPort) {
+                                         @JsonProperty("apiPort") int methodeApiPort,
+                                         @JsonProperty("jerseyClient") Optional<JerseyClientConfiguration> jerseyClientConfiguration) {
         this.methodeApiHost = methodeApiHost;
         this.methodeApiPort = methodeApiPort;
+        this.jerseyClientConfiguration = jerseyClientConfiguration.or(new JerseyClientConfiguration());
     }
 
     public String getMethodeApiHost() {
@@ -19,5 +26,10 @@ public class MethodeApiClientConfiguration {
 
     public int getMethodeApiPort() {
         return methodeApiPort;
+    }
+
+    @Valid
+    public JerseyClientConfiguration getJerseyClientConfiguration() {
+        return jerseyClientConfiguration;
     }
 }
