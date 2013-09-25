13e609c7ceceb56e798b8f4f33bfa4c8cd2676d5
Neil Green <neil@melessoftware.com>
2013-09-25 09:50:14 +0100

    create builder for EomFile

--- methode-api-model/src/main/java/com/ft/methodeapi/model/EomFile.java
+++ methode-api-model/src/main/java/com/ft/methodeapi/model/EomFile.java
@@ -4,9 +4,9 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class EomFile {
 
-    private String uuid;
-    private String type;
-    private byte[] value;
+    private final String uuid;
+    private final String type;
+    private final byte[] value;
 
 
     public EomFile(@JsonProperty("uuid") String uuid,
@@ -14,7 +14,7 @@ public class EomFile {
                    @JsonProperty("value") byte[] bytes) {
         this.uuid = uuid;
         this.type = type;
-        this.value = bytes; // yes. really.
+        this.value = bytes;
     }
 
     public String getUuid() {
@@ -27,5 +27,30 @@ public class EomFile {
 
     public byte[] getValue() {
         return value;
+    }
+
+    public static class Builder {
+        private String uuid;
+        private String type;
+        private byte[] value;
+
+        public Builder withUuid(String uuid) {
+            this.uuid = uuid;
+            return this;
+        }
+
+        public Builder withType(String type) {
+            this.type = type;
+            return this;
+        }
+
+        public Builder withValue(byte[] bytes) {
+            this.value = value;
+            return this;
+        }
+
+        public EomFile build() {
+            return new EomFile(uuid, type, value);
+        }
     }
 }
