3fc5e9594076ff6abb819df625826cf509e988f8
Neil Green <neil@melessoftware.com>
2013-09-25 12:05:46 +0100

    map attributes field

--- methode-api-model/src/main/java/com/ft/methodeapi/model/EomFile.java
+++ methode-api-model/src/main/java/com/ft/methodeapi/model/EomFile.java
@@ -1,20 +1,23 @@
 package com.ft.methodeapi.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.collect.ImmutableRangeSet;
 
 public class EomFile {
 
     private final String uuid;
     private final String type;
     private final byte[] value;
-
+    private final String attributes;
 
     public EomFile(@JsonProperty("uuid") String uuid,
                    @JsonProperty("type") String type,
-                   @JsonProperty("value") byte[] bytes) {
+                   @JsonProperty("value") byte[] bytes,
+                   @JsonProperty("attributes") String attributes) {
         this.uuid = uuid;
         this.type = type;
         this.value = bytes;
+        this.attributes = attributes;
     }
 
     public String getUuid() {
@@ -29,10 +32,15 @@ public class EomFile {
         return value;
     }
 
+    public String getAttributes() {
+        return attributes;
+    }
+
     public static class Builder {
         private String uuid;
         private String type;
         private byte[] value;
+        private String attributes;
 
         public Builder withUuid(String uuid) {
             this.uuid = uuid;
@@ -49,8 +57,13 @@ public class EomFile {
             return this;
         }
 
+        public Builder withAttributes(String attributes) {
+            this.attributes = attributes;
+            return this;
+        }
+
         public EomFile build() {
-            return new EomFile(uuid, type, value);
+            return new EomFile(uuid, type, value, attributes);
         }
     }
 }
