85f21d2af67f046ce12f923f64e7d228475a93e2
Simon.Gibbs <simon.gibbs@ft.com>
2013-10-16 11:52:50 +0100

    US36079: Add test for exception handling and coverage reporting. No threshold enforcement.

--- methode-api-service/src/test/java/com/ft/methodeapi/service/methode/MethodeObjectFactoryTest.java
+++ methode-api-service/src/test/java/com/ft/methodeapi/service/methode/MethodeObjectFactoryTest.java
@@ -1,16 +1,32 @@
 package com.ft.methodeapi.service.methode;
 
+import EOM.PermissionDenied;
+import EOM.Session;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.verify;
 
 /**
  * MethodeObjectFactoryTest
  *
  * @author Simon.Gibbs
  */
+@RunWith(MockitoJUnitRunner.class)
 public class MethodeObjectFactoryTest {
 
     private MethodeObjectFactory methodeObjectFactory;
+
+    @Mock
+    Session mockEomSession;
+
 
     @Before
     public void setupFactory() {
@@ -36,5 +52,32 @@ public class MethodeObjectFactoryTest {
     public void shouldNotFailWhenMaybeClosingNullSession() {
         methodeObjectFactory.maybeCloseSession(null);
     }
+
+    /**
+     * Trying to close a session that already has fundamental problems should not cause a new failure condition
+     */
+    @Test
+    public void shouldSwallowPermissionDeniedExceptionOnClosing() {
+
+        givenASessionPrimedToThrow(new PermissionDenied());
+
+        methodeObjectFactory.maybeCloseSession(mockEomSession);
+
+        try {
+            verify(mockEomSession).destroy();
+        } catch (Throwable t) {
+            assertThat(t, instanceOf(PermissionDenied.class));
+        }
+
+    }
+
+    private void givenASessionPrimedToThrow(Exception exception) {
+        try {
+            doThrow(exception).when(mockEomSession).destroy();
+        } catch (Throwable objectLocked) {
+            fail("Exception during mock object setup");
+        }
+    }
+
 
 }
