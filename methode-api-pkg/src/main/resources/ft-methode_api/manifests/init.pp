88233851ed3b2b7c145f400e20e287ece935df3f
root <root@varnish-1.(none)>
2013-11-13 12:43:52 +0100

    add nagios monitoring

--- methode-api-pkg/src/main/resources/ft-methode_api/manifests/init.pp
+++ methode-api-pkg/src/main/resources/ft-methode_api/manifests/init.pp
@@ -23,4 +23,13 @@ class methode_api {
         config_file_content => template('methode_api/config.yml.erb'),
         status_check_url => "http://localhost:8080/build-info";
     }
+
+}
+
+nagios::nrpe_checks::check_http{
+  "${::certname}/1": 
+    url      => "url: http://localhost/healthcheck",
+    port    => "8081",
+    expect => "OK",
+    notes   => "Methode API is not available";
 }
