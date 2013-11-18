5e6a845eefb199aca080b1fb30d5ee24588a7d5c
vaishakh.nadgir <vaishakh.nadgir@ft.com>
2013-11-18 16:04:39 +0000

    Using double-quotes to help interpretation of facter variables for hostname

--- methode-api-pkg/src/main/resources/ft-methode_api/manifests/monitoring/nagios.pp
+++ methode-api-pkg/src/main/resources/ft-methode_api/manifests/monitoring/nagios.pp
@@ -2,12 +2,12 @@ class methode_api::monitoring::nagios {
   $thisenv = hiera('lifecycle_environment','')
   nagios::nrpe_checks::check_http{
   "${::certname}/1":
-    url           => "http://localhost/healthcheck",
+    url           => "http://${::hostname}/healthcheck",
     port          => "8081",
     expect        => 'can search for content',
     size          => 1,
     action_url    => 'https://sites.google.com/a/ft.com/dynamic-publishing-team/home/methode-api',
-    notes         => 'Severity 1 \\n Service unavailable \\n Methode API healthchecks are failing consistently. Please check http://${::hostname}:8081/healthcheck \\n\\n',
+    notes         => "Severity 1 \\n Service unavailable \\n Methode API healthchecks are failing consistently. Please check http://${::hostname}:8081/healthcheck \\n\\n",
     ctime	  => '3.0';
   }
 }
