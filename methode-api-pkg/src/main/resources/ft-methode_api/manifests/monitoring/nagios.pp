9a37d9e593545ece26701e9f7c37d01d3ac30d1c
vaishakh.nadgir <vaishakh.nadgir@ft.com>
2013-11-18 14:29:04 +0000

    Added the healthcheck url to the message that gets sent out as part of the alerts

--- methode-api-pkg/src/main/resources/ft-methode_api/manifests/monitoring/nagios.pp
+++ methode-api-pkg/src/main/resources/ft-methode_api/manifests/monitoring/nagios.pp
@@ -7,7 +7,7 @@ class methode_api::monitoring::nagios {
     expect        => 'can search for content',
     size          => 1,
     action_url    => 'https://sites.google.com/a/ft.com/dynamic-publishing-team/home/methode-api',
-    notes         => 'Severity 1 \\n Service unavailable \\n Methode API healthchecks are failing consistently. Please check http://${hostname}:8081/healthcheck',
+    notes         => 'Severity 1 \\n Service unavailable \\n Methode API healthchecks are failing consistently. Please check http://${::hostname}:8081/healthcheck \\n\\n',
     ctime	  => '3.0';
   }
 }
