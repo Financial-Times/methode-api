9e47bfa245c601eeeea04787ecb99809d119a8c5
vaishakh.nadgir <vaishakh.nadgir@ft.com>
2013-11-19 11:11:56 +0000

    Updated the critical indicator threshold to 2.5 and warn indicator threshold to 1.0

--- methode-api-pkg/src/main/resources/ft-methode_api/manifests/monitoring/nagios.pp
+++ methode-api-pkg/src/main/resources/ft-methode_api/manifests/monitoring/nagios.pp
@@ -6,6 +6,8 @@ class methode_api::monitoring::nagios {
     port          => "8081",
     expect        => 'can search for content',
     size          => 1,
+    wtime         => 1.0,
+    ctime         => 2.5,
     action_url    => 'https://sites.google.com/a/ft.com/dynamic-publishing-team/home/methode-api',
     notes         => "Severity 1 \\n Service unavailable \\n Methode API healthchecks are failing consistently. Please check http://${::hostname}:8081/healthcheck \\n\\n",
     ctime	  => '3.0';
