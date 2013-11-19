d773bc39a9710775c760f0df7f55d09090c1a3ec
vaishakh.nadgir <vaishakh.nadgir@ft.com>
2013-11-19 12:03:51 +0000

    Removed duplicate variable ctime from nagios.pp

--- methode-api-pkg/src/main/resources/ft-methode_api/manifests/monitoring/nagios.pp
+++ methode-api-pkg/src/main/resources/ft-methode_api/manifests/monitoring/nagios.pp
@@ -7,10 +7,9 @@ class methode_api::monitoring::nagios {
     expect        => 'can search for content',
     size          => 1,
     wtime         => 1.0,
-    ctime         => 2.5,
     action_url    => 'https://sites.google.com/a/ft.com/dynamic-publishing-team/home/methode-api',
     notes         => "Severity 1 \\n Service unavailable \\n Methode API healthchecks are failing consistently. Please check http://${::hostname}:8081/healthcheck \\n\\n",
-    ctime	  => '3.0';
+    ctime	  => 2.5;
   }
 }
 
