e60d243a824da06b2181288523f2078a5cb0e882
Simon.Gibbs <simon.gibbs@ft.com>
2013-12-02 09:30:19 +0000

    Refactor for code duplication

--- methode-api-service/src/test/java/com/ft/methodeapi/service/http/GetAssetTypeResourceTest.java
+++ methode-api-service/src/test/java/com/ft/methodeapi/service/http/GetAssetTypeResourceTest.java
@@ -27,6 +27,7 @@ public class GetAssetTypeResourceTest extends ResourceTest {
 	private static final String VALID_UUID1 = "54307a12-37fa-11e3-8f44-002128161462";
 	private static final String VALID_UUID2 = "64307a12-37fa-11e3-8f44-002128161462";
 	private static final String MISSING_UUID = "74307a12-37fa-11e3-8f44-002128161462";
+
 	private MethodeFileRepository methodeFileRepository;
 
 	@Override
@@ -36,30 +37,35 @@ public class GetAssetTypeResourceTest extends ResourceTest {
 	}
 	
 	@Test
-	public void shouldReturn200AndReturnTypeInformationForSuccessPath () {
+	public void shouldReturn200AndReturnTypeInformationForSuccessPath() {
 		Set<String> assetIdentifiers = Sets.newHashSet(VALID_UUID1, VALID_UUID2);
-		EomAssetType eomAssetType1 = new EomAssetType.Builder().uuid(VALID_UUID1).type("EOM:CompoundStory").build();
-		EomAssetType eomAssetType2 = new EomAssetType.Builder().uuid(VALID_UUID2).type("EOM:CompoundStory").build();
+
+		EomAssetType eomAssetType1 = buildExampleCompoundStoryWith(VALID_UUID1);
+		EomAssetType eomAssetType2 = buildExampleCompoundStoryWith(VALID_UUID2);
+
 		Map<String, EomAssetType> methodeFileRepositoryResult = ImmutableMap.of(VALID_UUID1, eomAssetType1, VALID_UUID2, eomAssetType2);
 		when(methodeFileRepository.getAssetTypes(assetIdentifiers)).thenReturn(methodeFileRepositoryResult );
 		
-		final ClientResponse clientResponse = client().resource("/asset-type").type(MediaType.APPLICATION_JSON).post(ClientResponse.class, assetIdentifiers);
+		final ClientResponse clientResponse = exerciseEndpointWith(assetIdentifiers);
 		
 		assertThat("response", clientResponse, hasProperty("status", equalTo(200)));
 		Map<String, EomAssetType> result = clientResponse.getEntity(new GenericType<Map<String, EomAssetType>>() { });
 		assertThat("result1", result.get(VALID_UUID1), equalTo(eomAssetType1));
 		assertThat("result2", result.get(VALID_UUID2), equalTo(eomAssetType2));
 	}
-	
-	@Test
+
+
+    @Test
 	public void shouldReturn200EvenIfSomeLookupsFail () {
 		Set<String> assetIdentifiers = Sets.newHashSet(VALID_UUID1, MISSING_UUID);
-		EomAssetType eomAssetType1 = new EomAssetType.Builder().uuid(VALID_UUID1).type("EOM:CompoundStory").build();
+
+		EomAssetType eomAssetType1 = buildExampleCompoundStoryWith(VALID_UUID1);
 		EomAssetType eomAssetType2 = new EomAssetType.Builder().uuid(MISSING_UUID).error("Invalid URI").build();
+
 		Map<String, EomAssetType> methodeFileRepositoryResult = ImmutableMap.of(VALID_UUID1, eomAssetType1, MISSING_UUID, eomAssetType2);
 		when(methodeFileRepository.getAssetTypes(assetIdentifiers)).thenReturn(methodeFileRepositoryResult );
 		
-		final ClientResponse clientResponse = client().resource("/asset-type").type(MediaType.APPLICATION_JSON).post(ClientResponse.class, assetIdentifiers);
+		final ClientResponse clientResponse = exerciseEndpointWith(assetIdentifiers);
 		
 		assertThat("response", clientResponse, hasProperty("status", equalTo(200)));
 		Map<String, EomAssetType> result = clientResponse.getEntity(new GenericType<Map<String, EomAssetType>>() { });
@@ -69,9 +75,17 @@ public class GetAssetTypeResourceTest extends ResourceTest {
 	
 	@Test
 	public void shouldReturn400ForNoBodySupplied () {
-		Set<String> assetIdentifiers = null;
-		final ClientResponse clientResponse = client().resource("/asset-type/").type(MediaType.APPLICATION_JSON_TYPE).post(ClientResponse.class, assetIdentifiers);
+		final ClientResponse clientResponse = exerciseEndpointWith(null);
 		
 		assertThat("response", clientResponse, hasProperty("status", equalTo(400)));
 	}
+
+    private ClientResponse exerciseEndpointWith(Set<String> assetIdentifiers) {
+        return client().resource("/asset-type").type(MediaType.APPLICATION_JSON).post(ClientResponse.class, assetIdentifiers);
+    }
+
+    private EomAssetType buildExampleCompoundStoryWith(String uuid) {
+        return new EomAssetType.Builder().uuid(uuid).type("EOM:CompoundStory").build();
+    }
+
 }
