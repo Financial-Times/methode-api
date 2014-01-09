cc389b77d199303dc4a1b686654126f08affa1e9
vaishakh.nadgir <vaishakh.nadgir@ft.com>
2014-01-09 17:25:06 +0000

    rewrote the nagios check to use the basic nagios_service resource provided by nagios puppet module. This is so that we can evaluate having a certain threshold for max retries before deciding the service is broken

--- methode-api-pkg/src/main/resources/ft-methode_api/manifests/monitoring/nagios.pp
+++ methode-api-pkg/src/main/resources/ft-methode_api/manifests/monitoring/nagios.pp
@@ -1,16 +1,17 @@
 class methode_api::monitoring::nagios {
   $thisenv = hiera('lifecycle_environment','')
-  nagios::nrpe_checks::check_http{
-  "${::certname}/1":
-    url           => "http://${::hostname}/healthcheck",
-    port          => "8081",
-    recode        => "200",
-    expect        => 'can search for content',
-    size          => 1,
-    wtime         => 3.0,
-    action_url    => 'https://sites.google.com/a/ft.com/dynamic-publishing-team/home/methode-api',
-    notes         => "Severity 1 \\n Service unavailable \\n Methode API healthchecks are failing consistently. Please check http://${::hostname}:8081/healthcheck \\n\\n",
-    ctime	  => 3.0;
+  
+  @@nagios_service { 'check_http__app01-methode-api-pr-uk.content-platform.int.cloud.ft.com_ftlnx01829-lvpr-uk-int_8081_/healthcheck_can search for content_200_1_3.0_3.0':
+    ensure              => 'present',
+    action_url          => 'https://sites.google.com/a/ft.com/dynamic-publishing-team/home/methode-api',
+    check_command       => 'check_http!ftlnx01829-lvpr-uk-int!8081!/healthcheck!can search for content!200!1!3.0!3.0',
+    host_name           => 'app01-methode-api-pr-uk.content-platform.int.cloud.ft.com',
+    notes               => 'Severity 1 \n Service unavailable \n Methode API healthchecks are failing consistently. Please check http://ftlnx01829-lvpr-uk-int:8081/healthcheck \n\n',
+    notes_url           => 'https://sites.google.com/a/ft.com/ftplatform/5-monitoring/d-action-guidence',
+    service_description => 'HTTP: ftlnx01829-lvpr-uk-int:8081/healthcheck:can search for content:200:1:3.0:3.0 ',
+    target              => '/etc/nagios/nagios_service.cfg',
+    use                 => 'generic-service',
+    max_check_attempts  => '5';
   }
 }
 
