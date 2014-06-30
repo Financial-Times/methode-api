5ff81f1e80e7b996d6ae5f2aba35c5809e1d85cf
Mike Huniewicz <michal.huniewicz@ft.com>
2014-06-30 14:15:32 +0100

    Error code in a variable so that it can be printed.

--- methode-api-pkg/src/main/resources/scripts/deploy-to-forge.sh
+++ methode-api-pkg/src/main/resources/scripts/deploy-to-forge.sh
@@ -6,8 +6,9 @@ start_time=`date +%s`
 MODULEFULLPATH="${project.build.directory}/ft-methode_api"
 
 /usr/local/bin/forge-admin.py --publish --source $MODULEFULLPATH
-if [[ $? -ne 0 ]]; then
-    echo -e "Attempt to publish $MODULEFULLPATH failed with code $?.\n"
+ERROR_CODE=$?
+if [[ $ERROR_CODE -ne 0 ]]; then
+    echo -e "Attempt to publish $MODULEFULLPATH failed with code $ERROR_CODE.\n"
     exit 255
 fi
 echo ""
