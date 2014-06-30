2b0309eb92e4997081712f0c8240e5abbe6132b7
Mike Huniewicz <michal.huniewicz@ft.com>
2014-06-30 11:57:52 +0100

    Print error message in case of failure.

--- methode-api-pkg/src/main/resources/scripts/deploy-to-forge.sh
+++ methode-api-pkg/src/main/resources/scripts/deploy-to-forge.sh
@@ -4,5 +4,9 @@ start_time=`date +%s`
 MODULEFULLPATH="${project.build.directory}/ft-methode_api"
 
 /usr/local/bin/forge-admin.py --publish --source $MODULEFULLPATH
+if [[ $? -ne 0 ]]; then
+    echo -e "Attempt to publish $MODULEFULLPATH failed with code $?.\n"
+    exit 255
+fi
 echo ""
 exit 0
